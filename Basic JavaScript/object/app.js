bbf0bbdcfe9257f824d516816f7b0633b8c5b6b9
essram <esarama22@gmail.com>
2024-04-19 21:10:17 +0700

    Cara membuat struktur data pada JavaScript

--- Basic JavaScript/object/app.js
+++ Basic JavaScript/object/app.js
@@ -1,1 +1,19 @@
 //Cara Membuat Struktur Data Object
+const orang = {
+  nama: "Jug",
+  umur: 20,
+  alamat: "Jakarta",
+};
+
+const barang = {
+  barang: "Sepatu",
+  harga: 100000,
+  isReady: true,
+  size: ["38", "39", "40", "41", "42"],
+  location: {
+    indonesia: 48,
+    singapore: 50,
+  },
+};
+
+
