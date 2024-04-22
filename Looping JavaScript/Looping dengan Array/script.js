eee4975f313f0f8c908a23e31c5ade22798179b0
essram <esarama22@gmail.com>
2024-04-22 22:11:14 +0700

    Mendapatkan data array dengan perulangan/looping

---
+++ Looping JavaScript/Looping dengan Array/script.js
@@ -0,0 +1,8 @@
+// Mendapatkan Data Dari Array Dengan Perulangan
+
+const animals = ['cat', 'deer', 'hog', 'koala'];
+
+for (let i = 0; i < animals.length; i++) {
+    console.log(i, '-',  animals[i]);
+//     console.log(i + 1 , '-',  animals[i]);
+}
