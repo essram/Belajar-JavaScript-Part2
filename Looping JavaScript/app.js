da2551902bed0f80a8668099505b77e004275983
essram <esarama22@gmail.com>
2024-04-22 21:53:28 +0700

    Infinite Loop

--- Looping JavaScript/app.js
+++ Looping JavaScript/app.js
@@ -1,3 +1,8 @@
-for (let i = 10; i <= 0; i *= 10) {
+for (let i = 0; i <= 10; i++) {
   console.log("Hasil : " + i);
 }
+
+// Infinite loop
+// for (let i = 20; i>=0; i++) {
+//     console.log("Hasil : " + i);
+// }
