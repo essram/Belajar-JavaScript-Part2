8fd8aba01bf0f5d7d13b3b1c0e033e9080dbd27d
essram <esarama22@gmail.com>
2024-04-23 14:43:54 +0700

    Mendapatkan data dari Nested Array dengan Nested Loop

--- Looping JavaScript/Nested Loop/app.js
+++ Looping JavaScript/Nested Loop/app.js
@@ -11,10 +11,26 @@
 
 // Contoh 2
 
-const huruf = 'abcd';
-for (let i = 1; i <= 10; i++){
-    console.log(`${i}. Soal nomor ${i} :`);
-    for (let j = 0; j < huruf.length; j++){
-        console.log(`   ${huruf[j]}. Pilihan Jawaban`);
-    }
+// const huruf = 'abcd';
+// for (let i = 1; i <= 10; i++){
+//     console.log(`${i}. Soal nomor ${i} :`);
+//     for (let j = 0; j < huruf.length; j++){
+//         console.log(`   ${huruf[j]}. Pilihan Jawaban`);
+//     }
+// }
+
+// Mendapatkan Data Dari Nested Array Dengan Nested Loop
+
+const siswa = [
+   ['rama', 'budi', 'joko'],
+   ['asep', 'jono', 'joni'],
+   ['upin', 'ipin', 'opin']
+];
+
+for (let i = 0; i < siswa.length; i++){
+    const row = siswa[i];
+    console.log(`Tempat #${i + 1}`);
+    for(let j = 0; j < row.length; j++){
+        console.log(`   ${row[j]}`);
+}
 }
