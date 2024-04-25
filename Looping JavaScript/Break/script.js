54749b393158afde66b7f97405d6d400c9ccac64
essram <esarama22@gmail.com>
2024-04-25 22:01:37 +0700

    Break pada Looping

---
+++ Looping JavaScript/Break/script.js
@@ -0,0 +1,14 @@
+// let input = prompt("ketik sesuatu");
+
+// while (true) {
+//   input = prompt(input);
+//   if (input.toLowerCase() === "stop") break;
+// }
+
+// alert("sip")
+
+for (let i = 0; i < 200; i++) {
+    console.log(i);
+    if (i === 100)
+        break;
+}
