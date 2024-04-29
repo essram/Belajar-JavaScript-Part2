54b5116754dbd123838bc8e4082afca8ae922e56
essram <esarama22@gmail.com>
2024-04-29 17:21:56 +0700

    Update Array For Of

--- Looping JavaScript/Looping Pada Array For Of/script.js
+++ Looping JavaScript/Looping Pada Array For Of/script.js
@@ -1,5 +1,19 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// Array biasa
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-for (let buah of fruits) {
-    console.log(`buah ${fruits}`);
+// for (let buah of fruits) {
+//     console.log(`buah ${fruits}`);
+// }
+
+// Nested Array
+let studentRow = [
+    ['budi', 'asep', 'ucup'],
+    ['caca', 'cici', 'cucu'],
+    ['dede', 'dedi', 'dedu']
+]
+
+for( let row of studentRow) {
+    for (let student of row){  
+        console.log(student)
+    }
 }
