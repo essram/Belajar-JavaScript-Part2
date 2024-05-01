d33aecc68502464a35d5b9a9c28749bfd6b57cbe
essram <esarama22@gmail.com>
2024-05-01 09:01:43 +0700

    Update Function

--- Function & Method JavaScript/script.js
+++ Function & Method JavaScript/script.js
@@ -1,3 +1,13 @@
-function lemparDadu(){
-    console.log(Math.floor(Math.random() * 6) + 1);
+// function lemparDadu() {
+//   console.log(Math.floor(Math.random() * 6) + 1);
+// }
+// lemparDadu();
+
+// Define Function
+function nyanyi(){
+    console.log("cek...");
+    console.log("do re mi...");
 }
+
+// Call Function
+nyanyi();
