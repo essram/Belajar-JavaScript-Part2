db8ae0fa2f1230803bac3b4436eee9019bf7f566
essram <esarama22@gmail.com>
2024-05-03 23:22:54 +0700

    Lexical Scope pada Function

--- Function & Method JavaScript/Blocked Scope Variabel/script.js
+++
@@ -1,7 +0,0 @@
-let tinggi = 8;
-
-if (tinggi > 5) {
-  let lebar = 10;
-  console.log(lebar);
-}
-console.log(tinggi);
---
+++ Function & Method JavaScript/Scope pada function/Blocked Scope Variabel/script.js
@@ -0,0 +1,18 @@
+let tinggi = 8;
+
+if (tinggi > 5) {
+  var lebar = 10;
+//   console.log(lebar);
+}
+console.log("Lebar : ",lebar);
+
+for(let index = 0; index < 10; index++) {
+    var indexNumber = index;
+    // console.log(indexNumber);
+
+}
+console.log("Index : ",indexNumber)
+
+
+//Var tidak ada blocked Scope
+//let dan const ada blocked Scope
