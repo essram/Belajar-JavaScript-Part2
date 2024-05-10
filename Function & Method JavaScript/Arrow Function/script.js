1b0eaffdbf077b78d5328f8a398af5b272e06ae0
essram <esarama22@gmail.com>
2024-05-10 08:11:32 +0700

    Arrow Function

---
+++ Function & Method JavaScript/Arrow Function/script.js
@@ -0,0 +1,16 @@
+// Functionn Expression
+
+// const hasil = function perpangkatan (x){
+//     return x*x;
+// }
+
+// console.log(hasil(5))
+
+// Arrow Function
+const perpangkatan = (x) => {      // Kalau parameternya satu boleh tanpa menggunakan tutup kurung = const perpangkatan = x =>
+  return x * x;
+};
+
+console.log(perpangkatan(5));
+
+//
